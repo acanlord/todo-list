fdf5e6e0d2179e0bf5337d782adbbd5330e5c900
Richard Houck <rdhouck@gmail.com>
2020-02-04 21:27:25 -0800

    updated stuff

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Change Oil on Cars",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Feed the cats",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Fix the fence",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Mow lawn",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Clean the pool",
+        completed: false
+    }
+]
+
+export default todosData
