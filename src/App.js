37d3cb7dda9d2e49f52d9eb95fc1e0e32db4b513
Richard Houck <rdhouck@gmail.com>
2020-02-05 15:04:21 -0800

    updated stuff

--- src/App.js
+++ src/App.js
@@ -14,20 +14,24 @@ import todosData from "./todosData"
      }
 
      handleChange(id) {
-         this.setState(prevState => {
-             const updatededTodos = prevState.todos.map(todo => {
-                 if (todo.id === id) {
-                     todo.completed =! todo.completed
-                 }
-                 return todo
-             })
-             return {
-                 todos: updatededTodos
-             }
-         })
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })
+            console.log(prevState.todos)
+            console.log(updatedTodos)
+            return {
+                todos: updatedTodos
+            }
+        })
+    } 
 
-     }
-     
     render() {
         const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} 
         handleChange={this.handleChange}/>)
